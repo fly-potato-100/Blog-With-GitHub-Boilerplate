5333b46258c67bb27784eeaab7cd332c4fe76c51
fly-potato-100 <598181057@qq.com>
2020-01-08 15:54:22 +0800

    test: 测试配置文件

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "豆破苍穹"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+site_build_date = "2020-01-08T16:01+08:00"
+author = "飞奔的土豆"
+email = "sdyk88@gmail.com"
+author_homepage = "https://www.google.com"
+description = "不知道说点啥"
+key_words = ['Maverick', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
